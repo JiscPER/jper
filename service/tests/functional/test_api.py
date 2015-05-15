2c09185e8ea835742b4595c6be8696369a8e87a2
Richard Jones <richard@cottagelabs.com>
2015-05-15 10:39:39 +0100

    set up functional tests to start and stop their own test instance of the app

--- service/tests/functional/test_api.py
+++ service/tests/functional/test_api.py
@@ -1,30 +1,45 @@
-from unittest import TestCase
 import requests, json, os
 
-# from octopus.modules.es.testindex import ESTestCase
-from service import models
+from octopus.modules.es.testindex import ESTestCase
+from octopus.modules.test.helpers import get_first_free_port, TestServer, make_test_config
 from service.tests import fixtures
+from octopus.core import app
+from service import web
+from octopus.lib import paths
 
-API_BASE = "http://localhost:5024/api/v1/"
 API_KEY = "1234567890"
 
-class TestModels(TestCase):
+class TestAPI(ESTestCase):
     def setUp(self):
-        super(TestModels, self).setUp()
+        super(TestAPI, self).setUp()
+        self.config = {
+            "PORT" : get_first_free_port(),
+            "ELASTIC_SEARCH_INDEX" : app.config['ELASTIC_SEARCH_INDEX']
+        }
+        self.cfg_file = paths.rel2abs(__file__, "..", "resources", "test-server.cfg")
+
+        make_test_config(self.config, self.cfg_file)
+        self.test_server = TestServer(port=None, index=None, python_app_module_path=os.path.abspath(web.__file__), cfg_file=self.cfg_file)
+        self.test_server.spawn_with_config()
+
+        self.appurl = "http://localhost:{x}".format(x=self.config["PORT"])
+        self.api_base = self.appurl + "/api/v1/"
 
     def tearDown(self):
-        super(TestModels, self).tearDown()
+        super(TestAPI, self).tearDown()
+        self.test_server.terminate()
+        os.remove(self.cfg_file)
 
     def test_01_validation_singlepart(self):
         notification = fixtures.APIFactory.incoming()
-        url = API_BASE + "validate?api_key=" + API_KEY
+        url = self.api_base + "validate?api_key=" + API_KEY
         resp = requests.post(url, data=json.dumps(notification), headers={"Content-Type" : "application/json"})
         assert resp.status_code == 204
 
     def test_02_validation_multipart(self):
         notification = fixtures.APIFactory.incoming()
         example_package = fixtures.APIFactory.example_package_path()
-        url = API_BASE + "validate?api_key=" + API_KEY
+        url = self.api_base + "validate?api_key=" + API_KEY
         files = [
             ("metadata", ("metadata.json", json.dumps(notification), "application/json")),
             ("content", ("content.zip", open(example_package, "rb"), "application/zip"))
@@ -41,14 +56,14 @@ class TestModels(TestCase):
 
     def test_04_notification_singlepart(self):
         notification = fixtures.APIFactory.incoming()
-        url = API_BASE + "notification?api_key=" + API_KEY
+        url = self.api_base + "notification?api_key=" + API_KEY
         resp = requests.post(url, data=json.dumps(notification), headers={"Content-Type" : "application/json"})
         assert resp.status_code == 202
 
     def test_05_notification_multipart(self):
         notification = fixtures.APIFactory.incoming()
         example_package = fixtures.APIFactory.example_package_path()
-        url = API_BASE + "notification?api_key=" + API_KEY
+        url = self.api_base + "notification?api_key=" + API_KEY
         files = [
             ("metadata", ("metadata.json", json.dumps(notification), "application/json")),
             ("content", ("content.zip", open(example_package, "rb"), "application/zip"))
@@ -65,7 +80,7 @@ class TestModels(TestCase):
 
     def test_07_get_notification(self):
         notification = fixtures.APIFactory.incoming()
-        url = API_BASE + "notification?api_key=" + API_KEY
+        url = self.api_base + "notification?api_key=" + API_KEY
         resp = requests.post(url, data=json.dumps(notification), headers={"Content-Type" : "application/json"})
         url = resp.headers["location"]
         resp2 = requests.get(url)
@@ -80,7 +95,7 @@ class TestModels(TestCase):
     def test_09_get_store_content(self):
         notification = fixtures.APIFactory.incoming()
         example_package = fixtures.APIFactory.example_package_path()
-        url = API_BASE + "notification?api_key=" + API_KEY
+        url = self.api_base + "notification?api_key=" + API_KEY
         files = [
             ("metadata", ("metadata.json", json.dumps(notification), "application/json")),
             ("content", ("content.zip", open(example_package, "rb"), "application/zip"))
@@ -98,7 +113,7 @@ class TestModels(TestCase):
 
     def test_11_get_public_content(self):
         notification = fixtures.APIFactory.incoming()
-        url = API_BASE + "notification?api_key=" + API_KEY
+        url = self.api_base + "notification?api_key=" + API_KEY
         resp = requests.post(url, data=json.dumps(notification), headers={"Content-Type" : "application/json"})
         loc = resp.headers["location"]
         resp2 = requests.get(loc + "/content/1?api_key=" + API_KEY, allow_redirects=False)
@@ -111,7 +126,7 @@ class TestModels(TestCase):
         pass
 
     def test_13_list_all(self):
-        url = API_BASE + "routed?api_key=" + API_KEY + "&since=2001-01-01T00:00:00Z"
+        url = self.api_base + "routed?api_key=" + API_KEY + "&since=2001-01-01T00:00:00Z"
         resp = requests.get(url)
         assert resp.status_code == 200
 
@@ -125,7 +140,7 @@ class TestModels(TestCase):
         pass
 
     def test_15_list_repository(self):
-        url = API_BASE + "routed/repo1?api_key=" + API_KEY + "&since=2001-01-01T00:00:00Z"
+        url = self.api_base + "routed/repo1?api_key=" + API_KEY + "&since=2001-01-01T00:00:00Z"
         resp = requests.get(url)
         assert resp.status_code == 200
 
