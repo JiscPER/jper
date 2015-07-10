5cce31eeec6f82ab3d6ccf2d9c087c7c9c3532a8
Mark MacGillivray <mark@cottagelabs.com>
2015-07-10 09:58:06 +0100

    fixing login forms

--- service/views/account.py
+++ service/views/account.py
@@ -3,9 +3,6 @@ import uuid, json
 from flask import Blueprint, request, url_for, flash, redirect, make_response
 from flask import render_template, abort
 from flask.ext.login import login_user, logout_user, current_user
-from flask_wtf import Form
-from wtforms.fields import TextField, TextAreaField, SelectField, HiddenField, PasswordField
-from wtforms import validators, ValidationError
 
 from service import models
 
@@ -70,17 +67,17 @@ def username(username):
 @blueprint.route('/login', methods=['GET', 'POST'])
 def login():
 	if request.method == 'GET':
-		return render_template('account/login.html', form=form)
+		return render_template('account/login.html')
 	elif request.method == 'POST':
-		password = form.password.data
-		username = form.username.data
+		password = request.values['password']
+		username = request.values['username']
 		user = models.Account.pull(username)
 		if user is None:
 			user = models.Account.pull_by_email(username)
 		if user is not None and user.check_password(password):
 			login_user(user, remember=True)
 			flash('Welcome back.', 'success')
-			return form.redirect('index')
+			return render_template('account/login.html')
 		else:
 			flash('Incorrect username/password', 'error')
 
@@ -97,15 +94,14 @@ def register():
 	if not current_user.is_super:
 		abort(401)
 	elif request.method == 'GET':
-		return render_template('account/register.html', form=form)
+		return render_template('account/register.html')
 	elif request.method == 'POST':
 		api_key = str(uuid.uuid4())
 		account = models.Account(
-			id=form.w.data, 
-			email=form.n.data,
+			email=request.values['email'],
 			api_key=api_key
 		)
-		account.set_password(form.s.data)
+		account.set_password(request.values.password)
 		account.save()
 		flash('Account created for ' + account.id + '. If not listed below, refresh the page to catch up.', 'success')
 		return redirect('/account')
