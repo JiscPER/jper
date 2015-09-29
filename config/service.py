97a5551ee78006b6f8d0f223ce731a6635478551
Richard Jones <richard@cottagelabs.com>
2015-09-29 17:26:01 +0100

    fix API url for content urls as the notifications are initially created

--- config/service.py
+++ config/service.py
@@ -53,6 +53,7 @@ SECRET_KEY = "super-secret-key"
 # Service-specific config
 
 BASE_URL = "http://router2.mimas.ac.uk/"
+API_BASE_URL = BASE_URL + "api/v1/"
 
 DEFAULT_LIST_PAGE_START = 1
 DEFAULT_LIST_PAGE_SIZE = 25
