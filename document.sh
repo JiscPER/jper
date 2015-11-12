f9fbd955fd96963c852cc8127ee7f663ce552244
Richard Jones <richard@cottagelabs.com>
2015-11-12 10:43:26 +0000

    add initial epydoc generated documentation

---
+++ document.sh
@@ -0,0 +1,7 @@
+# In order to run this you need to have epydoc (http://epydoc.sourceforge.net/) installed, which can be done
+# on Ubuntu with
+#
+# sudo apt-get install python-epydoc
+
+rm docs/code/*
+epydoc --html -o docs/code/ --name "Jisc Publications Router" --url https://github.com/JiscPER/jper --graph all --inheritance grouped service
