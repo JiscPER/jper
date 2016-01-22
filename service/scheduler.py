74ae59351176bed585efa5b243d01c6334a1c5ef
Mark MacGillivray <mark@cottagelabs.com>
2016-01-22 22:25:53 +0000

    monthly reporting fix

--- service/scheduler.py
+++ service/scheduler.py
@@ -323,7 +323,7 @@ def monthly_reporting():
         app.logger.error("Scheduler - Failed scheduled reporting job: '{x}'".format(x=e.message))
   
 if app.config.get('SCHEDULE_MONTHLY_REPORTING',False):
-    schedule.every().day.at("22:20").do(monthly_reporting)
+    schedule.every().day.at("22:27").do(monthly_reporting)
 
 
 def cheep():
