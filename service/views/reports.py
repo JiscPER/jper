f1ef8b47bc76386e914f79494c601c83d69e64d0
Thomas Dierkes <tomdierkes@users.noreply.github.com>
2018-11-20 15:18:23 +0100

    Update service/views/reports.py
      Adding a date column to the download page offering all reports.
      This, of course, includes all adjustments to the html template
      'service/templates/reports/index.html' as well, in case you might ask.

--- service/views/reports.py
+++ service/views/reports.py
@@ -2,7 +2,7 @@
 Blueprint for providing reports UI
 """
 
-import os
+import os, time
 
 from flask import Blueprint, request, render_template, redirect, send_from_directory, flash
 from flask.ext.login import current_user
@@ -24,8 +24,14 @@ def restrict():
 def index():
     reportsdir = app.config.get('REPORTSDIR','/home/green/jper_reports')
     try:
-        fls = os.listdir(reportsdir)
-        reports = [fl for fl in fls if not fl.endswith('.cfg')]
+        #fls = os.listdir(reportsdir)
+        # 2018-11-20 TD : adding time stamp of last modification date to file list
+        fls = [] 
+        for f in os.listdir(reportsdir):
+            a = os.stat(os.path.join(reportsdir,f))
+            fls.append( (f,time.ctime(a.st_mtime)) )
+
+        reports = [(fl,mt) for (fl,mt) in fls if not fl.endswith('.cfg')]
     except:
         reports = []
     if len(reports) == 0: flash('There are currently no reports available','info')
