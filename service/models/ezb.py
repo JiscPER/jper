e2195dd921e680fbb842ab4f9e78f518e597230d
Thomas Dierkes <tomdierkes@users.noreply.github.com>
2018-12-20 08:49:03 +0100

    Update service/models/ezb.py
      Fixing the list of allowed type values in ES license DB scheme:
      Now 'alliance', 'national', 'open' and 'gold' are permitted.

--- service/models/ezb.py
+++ service/models/ezb.py
@@ -350,7 +350,7 @@ class License(dataobj.DataObj, dao.LicenseDAO):
                 "created_date" : {"coerce" : "utcdatetime"},
                 "last_updated" : {"coerce" : "utcdatetime"},
                 "name" : {"coerce" : "unicode"},
-                "type" : {"coerce" : "unicode", "allowed_values" : ["alliance", "national","open"]}
+                "type" : {"coerce" : "unicode", "allowed_values" : ["alliance", "national","open","gold"]}
             },
             # not (yet?) needed here
             # "objects" : [ 
@@ -454,8 +454,8 @@ class License(dataobj.DataObj, dao.LicenseDAO):
   
         :param val: the license package type
         """
-        if val not in ["alliance", "national", "other"]:
-            raise dataobj.DataSchemaException("license type must be one of 'alliance', 'national', or 'other'")
+        if val not in ["alliance", "national", "open", "gold"]:
+            raise dataobj.DataSchemaException("license type must be one of 'alliance', 'national', 'open' or 'gold'")
 
         self._set_single("type", val, coerce=dataobj.to_unicode())
 
