2d55276b676f50c3b7b67abe961a9c1ec3d163ff
Thomas Dierkes <tomdierkes@users.noreply.github.com>
2019-10-16 17:28:23 +0200

    New .../scripts/copynotifsfromstd2regular.py
      This little helper (re)assigns each routed notification so far
      collected from standard accounts to the corresponding regular
      account _if_ it is registered with DeepGreen.
    
      This shall provide a kind of service to repositories that came late,
      to offer them a certain stock of articles (if any!) ...

---
+++ service/scripts/copynotifsfromstd2regular.py
@@ -0,0 +1,102 @@
+# This Python file uses the following encoding: utf-8
+"""
+This script copies (i.e. reassigns) each routed notifications so far collected
+for standard accounts (i.e. hidden accounds in the background) to the corresponding
+regular account if it exists (i.e. is registered with DeepGreen!).
+
+"""
+try:
+    from octopus.core import add_configuration, app
+    from service.models import Account,RoutedNotification
+except:
+    print "ERROR: Need to run from a virtualenv enabled setting, i.e."
+    print "ERROR: run 'source ../../bin/activate' in some DG installation root folder first!"
+    exit(-1)
+
+import os
+
+def make_hidden2regular(hidden, regular={}):
+    hid2reg = {}
+    for x in hidden:
+        regids = []
+        ezbid = hidden[x][1:].upper()
+        if ezbid in regular.values():
+            regids = [ y for y in regular if ezbid == regular[y] ]
+        if len(regids) > 0:
+            if x in hid2reg:
+                regids = hid2reg[x] + regids
+            hid2reg[x] = list(set(regids))
+
+    return hid2reg
+
+
+def assign_hidnotes2regular(hid2reg={}, page_size=1000):
+    total = RoutedNotification.query(size=0).get('hits',{}).get('total',0)
+    if total <= 0:
+        print "ERROR: No routed notifications found."
+        return False
+
+    pages = (total / page_size) + 1
+
+    for page in xrange(pages):
+        frm = page*page_size
+        print "% 8d\r" % frm,
+        for raw in RoutedNotification.query(_from=frm,size=page_size).get('hits',{}).get('hits',[]):
+            if '_source' in raw:
+                typ = raw['_type']
+                note = RoutedNotification(raw['_source'])
+                repos = note.repositories
+                for rid in note.repositories:
+                    if rid in hid2reg:
+                        repos = repos + hid2reg[rid]
+                note.repositories = list(set(repos))
+                nrepos = len(note.repositories)
+                if nrepos > 1:
+                    note.reason = "Matched to {num} qualified repositories.".format(num=nrepos)
+
+                note.save(type=typ)
+
+    print
+    print "INFO: {total} routed notifications processed and adjusted.".format(total=total)
+
+    return True
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+
+    # some general script running features
+    parser.add_argument("-c", "--config", help="additional configuration to load (e.g. for testing)")
+
+    #parser.add_argument("-i", "--input", help="input .csv file")
+    parser.add_argument("-p", "--pagesize", help="page size of ES response to queries")
+    parser.add_argument("--run", action="store_true", help="a tiny but effective(!!) security switch")
+
+    args = parser.parse_args()
+
+    if args.run is not True:
+        print "ERROR: '--run switch is needed!"
+        print
+        exit(-1)
+
+    if args.config:
+        add_configuration(app, args.config)
+    
+    page_size = 1000
+    if args.pagesize is not None:
+        page_size = int(args.pagesize)
+
+    repos = Account.pull_all_by_key(key='role',value='repository')
+    hidden = { r.id: r.data['repository']['bibid'] for r in repos if r.data['repository']['bibid'].startswith('a') }
+    regular = { r.id: r.data['repository']['bibid'].upper() for r in repos if not r.data['repository']['bibid'].startswith('a') }
+
+    hid2reg = make_hidden2regular(hidden,regular)
+
+    if len(hid2reg) > 0:
+        rc = assign_hidnotes2regular(hid2reg=hid2reg, page_size=page_size)
+    else:
+        print "INFO: No std/hidden accounts found in ES to be processed, stop."
+
+    print
+    exit(0)
