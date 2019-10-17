abb534bef18012915e295b3e44049962e1a6e428
Thomas Dierkes <tomdierkes@users.noreply.github.com>
2019-10-17 09:42:33 +0200

    Update service/scripts/activate.py
      Add an option to activate / deactivate repository account IDs
      from a file (so-called 'bulk' option....).

--- service/scripts/activate.py
+++ service/scripts/activate.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     # some general script running features
     parser.add_argument("-d", "--debug", action="store_true", help="pycharm debug support enable")
     parser.add_argument("-c", "--config", help="additional configuration to load (e.g. for testing)")
+    parser.add_argument("-i", "--input", help="provide a .txt file (or .csv file with one column) of ids")
     parser.add_argument("-r", "--repo", help="id of the repository account to affect")
     parser.add_argument("-a", "--activate", help="activate this account (e.g. if it has been suspended)", action="store_true")
     parser.add_argument("-s", "--stop", help="stop/deactivate this account", action="store_true")
@@ -27,7 +28,7 @@ if __name__ == "__main__":
         pydevd.settrace(app.config.get('DEBUG_SERVER_HOST', 'localhost'), port=app.config.get('DEBUG_SERVER_PORT', 51234), stdoutToServer=True, stderrToServer=True)
         print "STARTED IN REMOTE DEBUG MODE"
 
-    if not args.repo:
+    if not args.repo and not args.input:
         parser.print_help()
         exit(0)
 
@@ -36,7 +37,24 @@ if __name__ == "__main__":
         parser.print_help()
         exit(0)
 
-    if args.stop:
-        control.deactivate_account(args.repo)
-    elif args.activate:
-        control.activate_account(args.repo)
+    if args.input and args.repo:
+        print "Please specify only one of -i/--input and -r/--repo"
+        parser.print_help()
+        exit(0)
+
+    if args.input:
+        try:
+            with open(args.input,'r') as f:
+                if args.stop:
+                    for line_repo_id in f:
+                        control.deactivate_account(line_repo_id)
+                elif args.activate:
+                    for line_repo_id in f:
+                        control.activate_account(line_repo_id)
+        except Exception as e:
+            pass
+    else:
+        if args.stop:
+            control.deactivate_account(args.repo)
+        elif args.activate:
+            control.activate_account(args.repo)
