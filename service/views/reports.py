b619c7a59779f358a37527fc04b9082b3b30b71f
Thomas Dierkes <tomdierkes@users.noreply.github.com>
2019-11-06 15:58:33 +0100

    Update service/views/reports.py
      Streamlined the initial generation of 'detailed_[routed|failed]_*.csv'
      files.  It is an awful hack (that hopefully never will break....).

--- service/views/reports.py
+++ service/views/reports.py
@@ -2,7 +2,7 @@
 Blueprint for providing reports UI
 """
 
-import os, time
+import os, time, re
 
 from flask import Blueprint, request, url_for, render_template, redirect, send_from_directory, flash
 from flask.ext.login import current_user
@@ -24,10 +24,12 @@ def restrict():
 @blueprint.route('/')
 def index():
     reportsdir = app.config.get('REPORTSDIR','/home/green/jper_reports')
+    tyear = int(time.strftime('%Y')
     try:
         #fls = os.listdir(reportsdir)
         # 2018-11-20 TD : adding time stamp of last modification date to file list
         # 2019-10-10 TD : adding line numbers (basically #records, since we have .csv)
+        # 2019-11-04 TD : adding year-month string from filename to file list
         fls = [] 
         for f in sorted(os.listdir(reportsdir)):
             # 2019-10-10 TD : count the lines of .csv files
@@ -35,15 +37,26 @@ def index():
             fname = os.path.join(reportsdir,f)
             lns = sum(1 for line in open(fname))
             a = os.stat(fname)
-            mth = 0
-            if f[-7] == '-':
-                mth = int(f[-6:-4])
+            # 2019-11-04 TD : extract date string '%Y-%m' (i.e. YYYY-mm) from file name (in f !)
+            yrm = re.findall(r'[12][0-9]{3}-[01][0-9]', f)
+            if not yrm:
+                yrm = '1900-00'
             # a = os.stat(os.path.join(reportsdir,f))
             # fls.append( (f, time.ctime(a.st_mtime)) )
-            fls.append( (f, time.strftime( '%F (%a) %T', time.localtime(a.st_mtime) ), lns, mth) )
+            fls.append( (f, time.strftime( '%F (%a) %T', time.localtime(a.st_mtime) ), lns, yrm) )
 
-        overall = [(fl,mt,nl,mn) for (fl,mt,nl,mn) in fls if not fl.endswith('.cfg') and fl.startswith('monthly')]
-        details = [(fl,mt,nl,mn) for (fl,mt,nl,mn) in fls if not fl.endswith('.cfg') and fl.startswith('detailed')]
+        overall = [(fl,mt,nl,ym) for (fl,mt,nl,ym) in fls if not fl.endswith('.cfg') and fl.startswith('monthly')]
+        details = [(fl,mt,nl,ym) for (fl,mt,nl,ym) in fls if not fl.endswith('.cfg') and fl.startswith('detailed') and ym.split('-')[0] == tyear]
+
+        if len(details) == 0:
+            for tmth in xrange(1,13):
+                fstem = "%04d-%02d.csv" % (tyear,tmth)
+                open(reportsdir + "/detailed_routed_notifications_" + fstem, 'w').close()
+                open(reportsdir + "/detailed_failed_notifications_" + fstem, 'w').close()
+
+            flash('Empty files for year {y} generated'.format(y=tyear),'info')
+            return redirect(url_for('.index'))
+
     except:
         overall = []
         details = []
@@ -58,16 +71,20 @@ def serve(filename):
     return send_from_directory(reportsdir, filename)
 
 
-@blueprint.route('/update/<month>')
-def refresh(month):
+# 2019-11-05 TD : parametrise route with a year and month (e.g. '2019-03' as yearmonth) 
+@blueprint.route('/update/<yearmonth>')
+def refresh(yearmonth):
     reportsdir = app.config.get('REPORTSDIR','/home/green/jper_reports')
     try:
         # flash('Updating reports, please be patient','info')
         # reports.admin_[routed|failed]_reports(frm_date,to_date,fname)
-        year = int(time.strftime('%Y'))
-        tmth = int(month)
+        #year = int(time.strftime('%Y'))
+        #tmth = int(month)
+        st = time.strptime(yearmonth,'%Y-%m')
+        year = st.tm_year
+        tmth = st.tm_mon
         ##for tmth in xrange(1,13):
-        if 0 < tmth and tmth < 13:
+        if 0 < tmth and tmth < 13 and 1900 < year:
             nxmth = (tmth+1) % 13
             frm_date = "%04d-%02d-01T00:00:00Z" % (year,tmth)
             fstem = "%04d-%02d.csv" % (year,tmth)
