199f4ecbfd46e720a9328625df61878fd828fbbd
kk <kkh900922@gmail.com>
2022-02-17 17:08:31 +0000

    add test cases

--- service/tests/unit/test_license_manage.py
+++ service/tests/unit/test_license_manage.py
@@ -1,5 +1,6 @@
+import io
 import time
-from typing import Iterable
+from typing import Iterable, Callable, Optional
 
 import flask
 import pkg_resources
@@ -10,6 +11,10 @@ from octopus.core import app
 from octopus.modules.es.testindex import ESTestCase
 from service.__utils import ez_dao_utils, ez_query_maker
 from service.models import Account, LicRelatedFile, License
+from service.views import license_manage
+
+PATH_LIC_A = '/tests/resources/new-EZB-NALIW-00493_AL.csv'
+PATH_PARTI_A = '/tests/resources/EZB-NALIW-00493_OA_participants_current.csv'
 
 
 def _is_incorrect_login(resp: flask.Response) -> bool:
@@ -29,6 +34,24 @@ def _login_as_admin(client):
     resp = _login(client, 'green@deepgreen.org', 'admin')
     if _is_incorrect_login(resp):
         raise Exception("Unexpected login fail")
+
+
+def do_upload_lic(client,
+                  file_path: str,
+                  lic_type: str = 'alliance',
+                  follow_redirects=False):
+    file_bytes: io.BufferedReader = pkg_resources.resource_stream('service', file_path)
+    lic_bytes = io.BytesIO(file_bytes.read())
+    lic_bytes.name = file_bytes.name.split('/')[-1]
+    post_data = {
+        'file': lic_bytes,
+        'lic_type': lic_type,
+    }
+
+    # run
+    resp: flask.Response = client.post('/license-manage/upload-license', data=post_data,
+                                       follow_redirects=follow_redirects)
+    return resp
 
 
 class LicDetailPageHelper:
@@ -136,11 +159,10 @@ class TestLicenseManage(ESTestCase):
         org_lrf_id_list = set(org_detail_page.non_active_lrf_id_list())
 
         # run
-        resp: flask.Response = self.do_upload(client, '/tests/resources/new-EZB-NALIW-00493_AL.csv',
-                                              lic_type='alliance', follow_redirects=True)
+        resp: flask.Response = do_upload_lic(client, PATH_LIC_A,
+                                             lic_type='alliance', follow_redirects=True)
 
         # assert
-
         new_size = LicRelatedFile.count(ez_query_maker.match_all())
         self.assertGreater(new_size, org_size)
 
@@ -167,68 +189,114 @@ class TestLicenseManage(ESTestCase):
         self.assertGreater(len(lic.identifiers), 0)
         self.assertEqual(lrf.ezb_id, lic.get_first_ezb_id())
 
-        # KTODO check file saved to hard disk
+        # check file saved to hard disk
+        # print(lrf.file_name)
+        self.assertTrue(license_manage.
+                        _path_lic_related_file(lrf.file_name)
+                        .is_file())
 
         # clean table
         lic.delete()
         lrf.delete()
 
-    def do_upload(self, client,
-                  file_path: str,
-                  lic_type: str = 'alliance',
-                  follow_redirects=False):
-        lic_bytes = pkg_resources.resource_stream('service', file_path)
-        post_data = {
-            'file': lic_bytes,
-            'lic_type': lic_type,
-        }
+    def test_upload_license__invalid_lic_file(self):
+        client = app.test_client()
+        _login_as_admin(client)
+
+        # prepare data for assert
+        org_detail_page = load_lic_detail_page_helper(client)
+
+        lrf_finder = create_new_lrf_finder()
 
         # run
-        resp: flask.Response = client.post('/license-manage/upload-license', data=post_data,
-                                           follow_redirects=follow_redirects)
-        return resp
+        resp: flask.Response = do_upload_lic(client, PATH_PARTI_A,
+                                             lic_type='alliance', follow_redirects=True)
+        self.assertEqual(resp.status_code, 400)
 
-    def test_upload_license__invalid_lic_file(self):  # KTODO
-        pass
+        time.sleep(3)  # wait for lrf saved
+        new_detail_page = load_lic_detail_page_helper(client)
+
+        # assert new history rows
+        self.assertGreater(new_detail_page.n_history_row(),
+                           org_detail_page.n_history_row())
+
+        # assert lrf
+        lrf = lrf_finder()
+        self.assertIsNotNone(lrf)
+        self.assertEqual(lrf.status, 'validation failed')
+        self.assertIsNotNone(lrf.data.get('validation_notes'))
+
+    def test_upload_license__duplicate_upload(self):  # KTODO
+        client = app.test_client()
+        _login_as_admin(client)
+
+        lrf_finder = create_new_lrf_finder()
+        resp: flask.Response = do_upload_lic(client, PATH_PARTI_A,
+                                             lic_type='alliance', follow_redirects=False)
+        lrf = lrf_finder()
 
     def test_active_lic_related_file__normal(self):
         client = app.test_client()
         _login_as_admin(client)
 
-        # org_lrf_id_list = {i.id for i in pull_all_by_status('validation passed')}
-        org_lrf_id_list = {i.id for i in LicRelatedFile.pull_all_by_query_str('status', 'validation passed')}
-
-        resp: flask.Response = self.do_upload(client, '/tests/resources/new-EZB-NALIW-00493_AL.csv',
-                                              lic_type='alliance', follow_redirects=True)
+        # prepare upload / validation passed record
+        new_lrf_finder = create_new_lrf_finder()
+        resp: flask.Response = do_upload_lic(client, PATH_LIC_A,
+                                             lic_type='alliance', follow_redirects=True)
         self.assertEqual(resp.status_code, 200)
-
-        new_lrf_id_list = {i.id for i in LicRelatedFile.pull_all_by_query_str('status', 'validation passed')}
-
-        diff_lrf_id_list = new_lrf_id_list - org_lrf_id_list
-        self.assertTrue(diff_lrf_id_list)
-        new_lrf_id = next(iter(diff_lrf_id_list))
-
-        new_lrf: LicRelatedFile = ez_dao_utils.pull_by_id(LicRelatedFile, new_lrf_id)
+        new_lrf = new_lrf_finder()
+        self.assertIsNotNone(new_lrf)
 
         # prepare data before run
+
+        # deactivate all lrf
+        for checker in [license_manage._deactivate_lrf_by_lrf_id(lrf.id, License) for lrf in
+                        LicRelatedFile.pull_all_by_status('active')]:
+            checker()
+
         org_detail_page = LicDetailPageHelper(resp.data)
+        incorrect_id_list = {l.id for l in License.pull_all_by_status_ezb_id('active', new_lrf.ezb_id)}
 
         # run
-        resp: Response = client.post('/license-manage/active-lic-related_file',
+        resp: Response = client.post('/license-manage/active-lic-related-file',
                                      data={'lrf_id': new_lrf.id, },
                                      follow_redirects=True)
         self.assertEqual(resp.status_code, 200)
-        new_detail_page = LicDetailPageHelper(resp.data)
 
-        self.assertGreater(
-            new_detail_page.n_active_row(),
-            org_detail_page.n_active_row(),
-        )
-        # KTODO
+        # assert new active lrf record in table
+        new_detail_page = LicDetailPageHelper(resp.data)
+        self.assertGreater(new_detail_page.n_active_row(),
+                           org_detail_page.n_active_row(), )
+
+        self.assertTrue(new_lrf.is_license())
+
+        # only one active record for each ezb_id
+        time.sleep(2)  # wait for License.save()
+        active_lic_id_list = {l.id for l in License.pull_all_by_status_ezb_id('active', new_lrf.ezb_id)}
+        active_lic_id_list = active_lic_id_list - incorrect_id_list
+        self.assertEqual(len(list(active_lic_id_list)), 1)
 
         # cleanup
         new_lrf.delete()
         ez_dao_utils.pull_by_id(License, new_lrf.record_id).delete()
+
+    def test_active_lic_related_file__auto_deactivate(self):  # KTODO
+        pass
+
+    def test_update_license__difference_ezb_id(self):  # KTODO
+        pass
+
+
+def create_new_lrf_finder() -> Callable[[], Optional[LicRelatedFile]]:
+    size = 1000
+    org_lrf_id_list = {lrf.id for lrf in ez_dao_utils.query_objs(LicRelatedFile, ez_query_maker.match_all(size=size))}
+
+    def _fn():
+        new_lrf_list = ez_dao_utils.query_objs(LicRelatedFile, ez_query_maker.match_all(size=size))
+        diff_lrf_list = [lrf for lrf in new_lrf_list if lrf.id not in org_lrf_id_list]
+        return diff_lrf_list[0] if diff_lrf_list else None
+
+    return _fn
 
 
 def pull_all_by_status(status):
